e16a961efee36fa8cfed35159dd0f14f0b7a41c2
Logan Smith <logsmith06@gmail.com>
2019-10-21 20:36:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+
+def prime?(num)
+  if num == 0 || num == 1 || num < 0
+    return false
+  end
+
+  pr = 2
+  div = num / pr
+  while pr <= div do
+    if num % pr == 0
+      return false
+    end
+    pr += 1
+    div = num / pr + 1
+  end
+return true
+
+
+end
