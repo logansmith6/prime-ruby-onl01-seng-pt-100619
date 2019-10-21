c1a21be5a066194313dd12772d52ca81689e8d0b
Logan Smith <logsmith06@gmail.com>
2019-10-21 20:34:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+
+def prime?(num)
+  if num == 0 || num == 1
+    return false
+  end
+
+  pr = 2
+  div = num / pr 
+  while pr < div do
+    if num % pr == 0
+      return false
+    end
+    pr += 1
+    div = num / pr 
+  end
+return true
+
+
+end
